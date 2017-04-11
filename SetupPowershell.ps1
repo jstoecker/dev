d56c1a952ee3f2e3f6eeaff9e8c9b861f4104073
Justin Stoecker <justin.w.stoecker@gmail.com>
2017-04-10 23:40:09 -0700

    Misc. updates

--- SetupPowershell.ps1
+++ SetupPowershell.ps1
@@ -1,15 +1,38 @@
+<#
+This class can be used to set colors for PowerShell windows.
+
+By default, colors for console-based executables are stored in the registry
+under HKEY_CURRENT_USER\Console. The values under this key apply to both
+cmd.exe and powershell.exe. However, there are PowerShell-specific values that
+override the values in HKCU\Console; these values are listed in the following
+subkeys (for a 64-bit version of the OS):
+
+x64: HKEY_CURRENT_USER\Console\%SystemRoot%_System32_WindowsPowerShell_v1.0_powershell.exe
+x86: HKEY_CURRENT_USER\Console\%SystemRoot%_SysWOW64_WindowsPowerShell_v1.0_powershell.exe
+
+The registry-stored color values do not apply when launching PowerShell from a
+shortcut (.lnk file), however. Individual shortcuts can store their own color
+properties, and these properties override whatever is set in the registry. If
+PowerShell is launched from the start menu (or by searching the start menu),
+then built-in shortcuts will override any colors that have been set in the
+registry.
+
+The class in this file can be used to set both the registry colors as well as
+replace the start menu shortcuts. This ensures the same colors are applied,
+regardless of how PowerShell is launched.
+#>
 class ConsoleColors
 {
     # Table of 16 colors with associated names. Each entry is initialized with
-    # Windows' default color values for a command prompt (not PowerShell).
+    # Windows' default color values for PowerShell.
     $ColorTable = [ordered]@{
         'Black'       = (  0,   0,   0); # index = 0
         'DarkBlue'    = (  0,   0, 128); # index = 1
         'DarkGreen'   = (  0, 128,   0); # index = 2
         'DarkCyan'    = (  0, 128, 128); # index = 3
         'DarkRed'     = (128,   0,   0); # index = 4
-        'DarkMagenta' = (128,   0, 128); # index = 5
-        'DarkYellow'  = (128, 128,   0); # index = 6
+        'DarkMagenta' = (  1,  36,  86); # index = 5
+        'DarkYellow'  = (238, 237, 240); # index = 6
         'Gray'        = (192, 192, 192); # index = 7
         'DarkGray'    = (128, 128, 128); # index = 8
         'Blue'        = (  0,   0, 255); # index = 9
@@ -26,13 +49,13 @@ class ConsoleColors
     [UInt16]$PopupBackground = 0xF # Popup text background color table index [0, 15]
     [UInt16]$PopupForeground = 0x3 # Popup text foreground color table index [0, 15]
 
-    [Int16]$ScreenBufferSizeX = 120
-    [Int16]$ScreenBufferSizeY = 3000
-    [Int16]$WindowSizeX = 120
-    [Int16]$WindowSizeY = 50
-    [Int16]$WindowOriginX = 0
-    [Int16]$WindowOriginY = 0
-    [UInt32]$FontSize = 786432 # 12pt
+    [UInt32]$ScreenBufferSizeX = 120
+    [UInt32]$ScreenBufferSizeY = 3000
+    [UInt32]$WindowSizeX = 120
+    [UInt32]$WindowSizeY = 50
+    [UInt32]$WindowOriginX = 0
+    [UInt32]$WindowOriginY = 0
+    [UInt32]$FontSize = 12
     [UInt32]$FontFamily = 32
     [UInt32]$FontWeight = 400
     [String]$FaceName = 'Lucida Console'
@@ -44,6 +67,11 @@ class ConsoleColors
     [UInt32]$HistoryBufferSize = 50
     [UInt32]$NumberOfHistoryBuffers = 4
     [UInt32]$HistoryNoDup = 0
+
+    # WindowAlpha is not the ConsoleDataBlock, but some new EXTRA_DATA block. This
+    # may or may not be documented, but I could inspect the hexdump to find the
+    # right byte to poke.
+    [UInt32]$WindowAlpha = 255
 
     # Changes the RGB values for the named color in the color table. For example,
     # SetColorValues('Black', 255, 0, 0) will result in the first color table entry
@@ -63,11 +91,14 @@ class ConsoleColors
         $This.FillForeground = [array]::IndexOf($This.ColorTable.Keys, $ColorName)
     }
 
-    # TODO additional setters
-
-    [void] ResetRegistryValues()
+    [void] SetFontSize([int]$FontSize)
     {
-        # TODO : back to default windows
+        $This.FontSize = $FontSize
+    }
+
+    [void] SetFaceName([string]$FaceName)
+    {
+        $This.FaceName = $FaceName
     }
 
     # Saves the console color properties to the Windows registry. The registry
@@ -76,14 +107,20 @@ class ConsoleColors
     # the console.
     [void] SaveRegistryValues()
     {
-        # TODO: apply to x86 as well?
         Push-Location -Path 'HKCU:\Console\%SystemRoot%_System32_WindowsPowerShell_v1.0_powershell.exe'
 
-        # TODO: apply all properties
+        Set-ItemProperty -Path . -Name ScreenColors -Type DWord -Value (($This.FillBackground -shl 4) -bor $This.FillForeground)
+        Set-ItemProperty -Path . -Name PopupColors -Type DWord -Value (($This.PopupBackground -shl 4) -bor $This.PopupForeground)
+        Set-ItemProperty -Path . -Name FontSize -Type DWord -Value ($This.FontSize -shl 16)
+        Set-ItemProperty -Path . -Name FaceName -Type String -Value $This.FaceName
 
-        # Pack ScreenColors (F)oreground and (B)ackground into 0x000000BF
-        $ScreenColors = ($This.FillBackground -shl 4) -bor $This.FillForeground
-        Set-ItemProperty -Path . -Name ScreenColors -Type DWord -Value $ScreenColors
+        Set-ItemProperty -Path . -Name ScreenBufferSize -Type DWord -Value (($This.ScreenBufferSizeY -shl 16) -bor $This.ScreenBufferSizeX)
+        Set-ItemProperty -Path . -Name WindowSize -Type DWord -Value (($This.WindowSizeY -shl 16) -bor $This.WindowSizeX)
+        # Set-ItemProperty -Path . -Name WindowPosition -Type DWord -Value (($This.WindowOriginX -shl 16) -bor $This.WindowOriginY)
+        Set-ItemProperty -Path . -Name WindowAlpha -Type DWord -Value $This.WindowAlpha
+
+        Set-ItemProperty -Path . -Name QuickEdit -Type DWord -Value $This.QuickEdit
+        Set-ItemProperty -Path . -Name InsertMode -Type DWord -Value $This.InsertMode
 
         for ($i = 0; $i -lt $This.ColorTable.Count; $i++)
         {
@@ -160,22 +197,19 @@ class ConsoleColors
                 $ColorTableBytes[$ByteIndex++] = 0
             }
 
-            [UInt16]$FillAttributes = ($This.FillBackground -shl 4) -bor $This.FillForeground
-            [UInt16]$PopupFillAttributes = ($This.PopupBackground -shl 4) -bor $This.PopupForeground
-
             $Writer.Write([UInt32]'0x000000CC') # BlockSize
             $Writer.Write([UInt32]'0xA0000002') # BlockSignature
-            $Writer.Write($FillAttributes)
-            $Writer.Write($PopupFillAttributes)
-            $Writer.Write($This.ScreenBufferSizeX)
-            $Writer.Write($This.ScreenBufferSizeY)
-            $Writer.Write($This.WindowSizeX)
-            $Writer.Write($This.WindowSizeY)
-            $Writer.Write($This.WindowOriginX)
-            $Writer.Write($This.WindowOriginY)
+            $Writer.Write([UInt16](($This.FillBackground -shl 4) -bor $This.FillForeground))
+            $Writer.Write([UInt16](($This.PopupBackground -shl 4) -bor $This.PopupForeground))
+            $Writer.Write([UInt16]$This.ScreenBufferSizeX)
+            $Writer.Write([UInt16]$This.ScreenBufferSizeY)
+            $Writer.Write([UInt16]$This.WindowSizeX)
+            $Writer.Write([UInt16]$This.WindowSizeY)
+            $Writer.Write([UInt16]$This.WindowOriginX)
+            $Writer.Write([UInt16]$This.WindowOriginY)
             $Writer.Write([UInt32]0) # Unused 1
             $Writer.Write([UInt32]0) # Unused 2
-            $Writer.Write($This.FontSize)
+            $Writer.Write(($This.FontSize -shl 16))
             $Writer.Write($This.FontFamily)
             $Writer.Write($This.FontWeight)
             $Writer.Write($FaceNameBytes)
@@ -201,6 +235,7 @@ class ConsoleColors
 }
 
 $ConsoleColors = [ConsoleColors]::new()
+$ConsoleColors.SetFontSize(14)
 $ConsoleColors.SetForegroundColor('White')
 $ConsoleColors.SetBackgroundColor('Black')
 $ConsoleColors.SetColorValues('Red', 230, 64, 64)
@@ -215,4 +250,10 @@ $ConsoleColors.SetColorValues('DarkBlue', 32, 32, 140)
 $ConsoleColors.SetColorValues('DarkCyan', 32, 140, 150)
 $ConsoleColors.SetColorValues('DarkMagenta', 32, 140, 140)
 $ConsoleColors.SetColorValues('DarkYellow', 140, 140, 32)
+$ConsoleColors.SaveRegistryValues()
 $ConsoleColors.CreateShortcut("$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows PowerShell.lnk")
+
+if (!(Get-Module PSColor))
+{
+    Install-Module PSColor
+}
