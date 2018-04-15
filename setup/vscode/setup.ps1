26aa2161cc90cbdf2efbe5f2d0c5764b82d31ec1
Justin Stoecker <justin.w.stoecker@gmail.com>
2018-04-14 19:07:00 -0700

    Update vscode setup script

--- setup/vscode/setup.ps1
+++ setup/vscode/setup.ps1
@@ -1,20 +1,26 @@
+# Download VSCode installer.
+$SetupPath = "$env:USERPROFILE\Downloads\VSCodeSetup-x64.exe"
+if (!(Test-Path $SetupPath))
+{
+    Write-Host "- Downloading VSCode to $SetupPath"
+    [Net.WebClient]::new().DownloadFile('https://go.microsoft.com/fwlink/?Linkid=852157', $SetupPath)
+}
 
+# Run VSCode setup, and refresh the PATH once it finishes.
+Write-Host "- Running VSCode setup"
+Start-Process $SetupPath -Wait
+$env:PATH = [System.Environment]::GetEnvironmentVariable('PATH', 'Machine') + ';' + 
+    [System.Environment]::GetEnvironmentVariable('PATH', 'User')
 
-# code --list-extensions
-# https://go.microsoft.com/fwlink/?Linkid=852157
-# Invoke-WebReqest -Url https://go.microsoft.com/fwlink/?Linkid=852157 -OutFile vscode-setup.exe
-
+# Install extensions.
 $Extensions = 'zhuangtongfa.Material-theme',
-              'twxs.cmake',
-              'ms-vscode.cpptools',
-              'ms-vscode.PowerShell',
-              'ms-vscode.sublime-keybindings'
+    'twxs.cmake',
+    'ms-vscode.cpptools',
+    'ms-vscode.PowerShell',
+    'ms-vscode.sublime-keybindings'
 
 $Extensions | ForEach-Object { code --install-extension $_ }
 
-
-# Download VSCode
-# Install VSCode
-# Add to PATH
-# Install extnesions
-# Copy settings.json
+# Global configuration.
+# Copy-Item "$PSScriptRoot\.gitconfig" "$env:USERPROFILE\.gitconfig"
+# git config --global --edit
