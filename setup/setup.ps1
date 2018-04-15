978d8d26d2eff083cafbe059e876d906fe4f616f
Justin Stoecker <justin.w.stoecker@gmail.com>
2018-04-15 13:01:24 -0700

    Update main setup script to use choco.

--- setup/setup.ps1
+++ setup/setup.ps1
@@ -1,22 +1,13 @@
-# Run setup.ps1 in all subdirectories.
-# & "$PSScriptRoot\powershell\setup.ps1"
-# & "$PSScriptRoot\vscode\setup.ps1"
-# & "$PSScriptRoot\git\setup.ps1"
-
-# TODO: CMake, VS, NuGet, VIM?
-
-# Install choco
-
-# 7zip?
-
-# choco install 
-
+# Install chocolatey.
 if (!(Get-Command choco -ErrorAction Ignore))
 {
     Set-ExecutionPolicy Bypass -Scope Process -Force
     Invoke-Expression "$([Net.WebClient]::new().DownloadString('https://chocolatey.org/install.ps1'))"
 }
 
-choco install visualstudiocode --confirm --params "/NoDesktopIcon"
+& "$PSScriptRoot\powershell\setup.ps1"
+& "$PSScriptRoot\vscode\setup.ps1"
+& "$PSScriptRoot\git\setup.ps1"
 
-
+choco install cmake
+choco install 7zip
